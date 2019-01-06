2369cd659449eadeff14ff4e74463049a147c547
IrshaadS <Irshaad_S@rediffmail.com>
2019-01-06 23:47:27 +0530

    Implementing Service Interfaces for POJOs

---
+++ pet-clinic-data/src/main/java/com/sfgpetclinic/services/PetService.java
@@ -0,0 +1,17 @@
+package com.sfgpetclinic.services;
+
+import com.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+
+
+
+}
---
+++ pet-clinic-data/src/main/java/com/sfgpetclinic/services/VetService.java
@@ -0,0 +1,15 @@
+package com.sfgpetclinic.services;
+
+import com.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+
+}
