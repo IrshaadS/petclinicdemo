b3e62e90e98e25fb026df5e753734e3bec1bee68
IrshaadS <Irshaad_S@rediffmail.com>
2019-01-07 12:51:31 +0530

    Added Vets Index page and Vets Index Controller

---
+++ pet-clinic-web/src/main/java/com/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,18 @@
+// Created by: Irshad Shaikh 
+// On: 1/7/2019
+
+package com.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets(){
+
+
+        return "vets/index";
+    }
+
+}
